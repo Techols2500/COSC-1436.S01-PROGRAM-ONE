915227f3206b3a760ebd924dca4fa4ce4cf21a24
Tyler Echols <87506869+Techols2500@users.noreply.github.com>
2023-03-09 13:28:40 -0600

    Add files via upload

---
+++ Assignment 1 .c
@@ -0,0 +1,20 @@
+/**************************************************************************
+DESCRIPTION: Programming Exercise One
+CLASS:   COSC 1420.S01
+AUTHOR:   Tyler Echols
+DATE:    Jan 2019
+**************************************************************************/
+
+#pragma warning (disable:4996)
+
+#include <stdio.h>  
+
+int main()
+{
+	char x;
+	printf("First program by Tyler Echols for COSC 1420.S01\n");
+	printf("Hit any key to move on");
+	scanf("%x", &x);
+
+	return 0;
+}
---
+++ Assignment.c
@@ -0,0 +1,20 @@
+/*
+DESCRIPTION: Programming Excerise One
+CLASS: COSC 1420.SO1 
+AUTHOR: TYLER ECHOLS 
+DATE: Jan 2019 
+*/
+
+#pragma warning (disable:4996)
+
+#include <stdio.h>  
+
+int main()
+{
+	char x; 
+	printf ("Frist program by Tyler Echols for COSC 1420.S01"); 
+	printf ("Hit any key to move on");  
+	scanf("%x", &x);
+
+		return 0; 
+}
